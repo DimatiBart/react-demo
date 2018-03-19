df907661497775babfbf86548bf9b52f1a6c3f08
Dzmitry Bartalevich <dimati.bart@gmail.com>
2018-03-19 17:40:11 +0300

    added real rendering with router except broke static files

--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@ import fetch from "isomorphic-fetch";
 import React from "react";
 import ReactDOMServer from "react-dom/server";
 
+import { AppContainer } from 'react-hot-loader'
+import { StaticRouter } from 'react-router-dom'
+
+import App from "../src/App";
+
+import renderPage from "./page";
+
 
 process.title = "ssr-server";
 global.fetch = fetch;
@@ -13,7 +20,19 @@ server.listen(1345, function() {
     console.log('%s listening at %s', server.name, server.url);
 });
 
+const renderApp = (url) => {
+    let context = {};
+    return ReactDOMServer.renderToString(
+        <AppContainer>
+            <StaticRouter location={url} context={context}>
+                <App/>
+            </StaticRouter>
+        </AppContainer>,
+    )
+};
+
 server.get(/.?/, (req, res) => {
     console.log(req.path());
-    res.sendRaw(ReactDOMServer.renderToString(<h1>Hello, two plus two is {2 + 2}, current path is {req.path()}</h1>));
+    let renderedApp = renderApp(req.path());
+    res.sendRaw( renderPage(renderedApp) );
 });
---
+++ server/page.js
@@ -0,0 +1,22 @@
+export default (App) => (
+    `<!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>React demo</title>
+        <script defer src="./dest/js/main.bundle.js"></script>
+        <style>
+            .hoho {
+                display: block;
+                margin: 10px;
+            }
+        </style>
+    </head>
+    <body>
+    <div id="app">${App}</div>
+    </body>
+    </html>`
+)
