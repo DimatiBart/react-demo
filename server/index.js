3d0ba61d8c6bfb4ffd971923232029ac217fd325
Dzmitry Bartalevich <dimati.bart@gmail.com>
2018-03-19 17:20:08 +0300

    added basic structure of ssr example

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import restify from "restify";
+import fetch from "isomorphic-fetch";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+
+
+process.title = "ssr-server";
+global.fetch = fetch;
+
+let server = restify.createServer();
+
+server.listen(1345, function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
+
+server.get(/.?/, (req, res) => {
+    console.log(req.path());
+    res.sendRaw(ReactDOMServer.renderToString(<h1>Hello, two plus two is {2 + 2}, current path is {req.path()}</h1>));
+});
